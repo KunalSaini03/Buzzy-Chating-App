399b5fbb8c4a10e652cbe3ebd0472904939454f8
Kunal Saini <130821265+KunalSaini03@users.noreply.github.com>
2024-03-13 21:47:39 +0530

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Buzzy",
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
       theme: Provider.of<ThemeProvider>(context).themeData,
